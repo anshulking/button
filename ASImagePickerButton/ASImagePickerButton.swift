d1bcc2633f8485cba652ebfa95e1c028ab3da374
MAC238 <MAC238@Anshul-MAC238.local>
2017-07-13 17:53:49 +0530

    first commit

---
+++ ASImagePickerButton/ASImagePickerButton.swift
@@ -0,0 +1,21 @@
+//
+//  ASImagePickerButton.swift
+//  ASImagePickerButton
+//
+//  Created by MAC238 on 7/13/17.
+//  Copyright © 2017 tatvasoft. All rights reserved.
+//
+
+import UIKit
+
+class ASImagePickerButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
